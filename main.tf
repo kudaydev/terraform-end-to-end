2d262f579d8d8d373602b128fab95b05b9270c43
kudaydev <kuday.mail9@gmail.com>
2024-01-26 12:54:53 +0530

    end to end test

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# Configure and downloading plugins for aws
-provider "aws" {
-  region     = "${var.aws_region}"
-}
-
 # Creating VPC
 resource "aws_vpc" "demovpc" {
   cidr_block       = "${var.vpc_cidr}"
